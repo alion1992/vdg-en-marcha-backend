74ab5bd4605827bb59d8d73a2fcbf595a430b774
alion1992 <55086590+alion1992@users.noreply.github.com>
2026-06-09 08:38:52 +0200

    cambio de dto

--- src/main/java/vdg/marcha/puertollano/dto/LoginRequest.java
+++ src/main/java/vdg/marcha/puertollano/dto/LoginRequest.java
@@ -3,14 +3,16 @@ package vdg.marcha.puertollano.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
-    @NotBlank
     private String dni;
 
-    @NotBlank
     private String password;
 }
